b0320da0d0a07a3cc4c042b33601445c291dd5a5
Rob Shovan <robert.c.shovan@gmail.com>
2024-09-01 13:19:36 -0400

    gather sale velocity

---
+++ README.md
@@ -0,0 +1,7 @@
+https://docs.universalis.app/
+
+https://github.com/CameronDeweerd/FFXIV-Market-Calculator
+
+
+https://xivapi.com/docs
+https://xivapi.com/docs/Icons
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import time
 import requests
 
 # config
@@ -19,6 +20,79 @@ def jprint(obj):
     print(text)
 
 def main():
+    
+    items_list = []
+    with open('marketable.json') as f:
+        d = json.load(f)
+        counter = 0
+        items = ""
+        for i in d:
+            if counter == 99:
+                items += str(i)
+                items_list.append(items)
+                items = ""
+                counter = 0
+                continue
+            
+            items += str(i) + ','
+            counter += 1
+    
+    #print(items_list)
+    out_list = []
+    
+    for items in items_list:
+        uri = f'https://universalis.app/api/v2/aggregated/{dc_name}/{items}'
+        response = requests.get(uri)
+        j = response.json()["results"]
+        for e in j:
+            id = e["itemId"]
+            v = ""
+            try:
+                v = e["nq"]["dailySaleVelocity"]["dc"]["quantity"]
+            except Exception as ex:
+                # couldn't find velocity for dc specifically, just skip
+                continue
+            #print(f'id: {id} | v: {v}')
+            out_list.append({"itemId": id, "dailySaleVelocity": v})
+        time.sleep(.05)
+    
+    json_obj = json.dumps(out_list, indent=4)
+    with open("data.json", "w") as outfile:
+        outfile.write(json_obj)
+    
+    '''
+    out_list = []
+    
+    items = "44300,44301,44302"
+    
+    #uri = f'/api/v2/aggregated/{worldDcRegion}/{itemIds}'
+    uri = f'https://universalis.app/api/v2/aggregated/{dc_name}/{items}'
+    response = requests.get(uri)
+    #print(response.json())
+    j = response.json()["results"]
+    #print(j)
+    for e in j:
+        id = e["itemId"]
+        v = e["nq"]["dailySaleVelocity"]["dc"]["quantity"]
+        #print(f'id: {id} | v: {v}')
+        out_list.append({"itemId": id, "dailySaleVelocity": v})
+
+    #print(out_list)
+    
+    # wait .05 seconds between requests
+    
+    
+    
+    json_obj = json.dumps(out_list, indent=4)
+    with open("data.json", "w") as outfile:
+        outfile.write(json_obj)
+    '''
+    
+    
+    
+    
+    
+    
     '''
     # get NA world ids
     na_worlds_ids = []
